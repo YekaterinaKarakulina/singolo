de338e80b43896138f9d896c211e9146c2bbedbf
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-11 17:22:43 +0600

    feat: add phone background image turn on/off

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ const navigation = document.getElementById('navigation-list');
 
 navigation.addEventListener('click', (event) => {
     navigation.querySelectorAll('li').forEach(element => element.classList.remove('active'));
-    console.log(event.target);
-    console.log(event.target.parentElement);
     event.target.parentElement.classList.add('active');
 });
+
+
+const phone__base__vertical = document.getElementById('phone__base__vertical');
+const phone__background__vertical = document.getElementById('phone__background__vertical');
+const phone__base__horizontal = document.getElementById('phone__base__horizontal');
+const phone__background__horizontal = document.getElementById('phone__background__horizontal');
+var flag_phone_vertical = false;
+var flag_phone_horizontal = false;
+
+phone__base__vertical.addEventListener('click', ()=> {
+    if(flag_phone_vertical) {
+        phone__background__vertical.classList.remove('hidden');
+        flag_phone_vertical = false;
+    } else {
+        phone__background__vertical.classList.add('hidden');
+        flag_phone_vertical = true;
+    }
+});
+
+phone__base__horizontal.addEventListener('click', ()=> {
+    if(flag_phone_horizontal) {
+        phone__background__horizontal.classList.remove('hidden');
+        flag_phone_horizontal = false;
+    } else {
+        phone__background__horizontal.classList.add('hidden');
+        flag_phone_horizontal = true;
+    }
+});
+
+
+
